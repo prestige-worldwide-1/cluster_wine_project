e1ca4a6555cae3f54c9cad0be38a31b4cc49361a
Martin Reyes <martin.anthony.reyes@gmail.com>
2023-07-20 12:54:33 -0500

    split function added in wrangle

--- wrangle.py
+++ wrangle.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def wrangle():
     
@@ -19,3 +20,25 @@ def wrangle():
     
     df.to_csv('wine_data.csv', index=False)
     return df
+
+def split_data(df, test_size=.15, validate_size=.15, stratify_col=None, random_state=None):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames;
+    return train, validate, test DataFrames.
+    '''
+    # no stratification
+    if stratify_col == None:
+        # split test data
+        train_validate, test = train_test_split(df, test_size=test_size, random_state=random_state)
+        # split validate data
+        train, validate = train_test_split(train_validate, test_size=validate_size/(1-test_size),
+                                                                           random_state=random_state)
+    # stratify split
+    else:
+        # split test data
+        train_validate, test = train_test_split(df, test_size=test_size,
+                                                random_state=random_state, stratify=df[stratify_col])
+        # split validate data
+        train, validate = train_test_split(train_validate, test_size=validate_size/(1-test_size),
+                                           random_state=random_state, stratify=train_validate[stratify_col])       
+    return train, validate, test
